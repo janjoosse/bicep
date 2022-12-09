e3757fc8f68b5d28f7d03cc888a5793846c93235
Jan Joosse <j.joosse@rijkzwaan.nl>
2022-12-09 17:36:56 +0100

    Refactor tests

--- src/Bicep.Core.UnitTests/Diagnostics/LinterRuleTests/MaxLengthModuleNameRuleTests.cs
+++ src/Bicep.Core.UnitTests/Diagnostics/LinterRuleTests/MaxLengthModuleNameRuleTests.cs
@@ -20,16 +20,23 @@ namespace Bicep.Core.UnitTests.Diagnostics.LinterRuleTests
             ruleToTest.GetMessage(100, 64).Should().Be("The name for this module has a length of 100, which exceeds the maximum of 64.");
         }
 
+        [DataRow(@"
+            module m1 'module1.bicep' = {
+                name: '64characterslooooooooooooooooooooooooooooooooooooooooooooongname'
+                scope: subscription()
+            }
+        ", false)]
+        [DataRow(@"
+            module m1 'module1.bicep' = {
+                name: '65charactersloooooooooooooooooooooooooooooooooooooooooooooongname'
+                scope: subscription()
+            }
+        ", true)]
         [DataTestMethod]
-        public void If_ModuleNameLength_UnderMaxLength_ShouldPass()
+        public void Rule_Should_FailOrPass_AsExpected(string bicepThatUsesModule, bool shouldFailOnRule)
         {
             var result = CompilationHelper.Compile(
-                ("main.bicep", @"
-                    module m1 'module1.bicep' = {
-                      name: 'ashortmodulename'
-                      scope: subscription()
-                    }
-                "),
+                ("main.bicep", bicepThatUsesModule),
                 ("module1.bicep", @"
                     targetScope = 'subscription'
                     resource rg 'Microsoft.Resources/resourceGroups@2021-04-01' = {
@@ -39,32 +46,18 @@ namespace Bicep.Core.UnitTests.Diagnostics.LinterRuleTests
                 ")
             );
 
-            result.Diagnostics.Should().BeEmpty();
-        }
-
-        [DataTestMethod]
-        public void If_ModuleNameLength_AboveMaxLength_ShouldFail()
-        {
-            var result = CompilationHelper.Compile(
-                ("main.bicep", @"
-                    module m1 'module1.bicep' = {
-                      name: 'averyloooooooooooooooooooooooooooooooooooooooooooooooooooooooooongname'
-                      scope: subscription()
-                    }
-                "),
-                ("module1.bicep", @"
-                    targetScope = 'subscription'
-                    resource rg 'Microsoft.Resources/resourceGroups@2021-04-01' = {
-                      name: 'name'
-                      location: 'global'
-                    }
-                ")
-            );
-
-            result.Diagnostics.Should().HaveDiagnostics(new[]
+            if (shouldFailOnRule)
             {
-                (MaxLengthModuleNameRule.Code, DiagnosticLevel.Error, "The name for this module has a length of 70, which exceeds the maximum of 64."),
-            });
+                result.Diagnostics.Should().HaveDiagnostics(new[]
+                {
+                    (MaxLengthModuleNameRule.Code, DiagnosticLevel.Error, "The name for this module has a length of 65, which exceeds the maximum of 64."),
+                });
+            }
+
+            else
+            {
+                result.Diagnostics.Should().BeEmpty();
+            }
         }
     }
 }
